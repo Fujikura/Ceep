1b0d31d16e82e697d6f82a1c55b886bf9f1b436e
Marcio <marciofujikura@hotmail.com>
2020-02-09 23:56:24 -0300

    Criando activity Lista de notas e listando a primeira nota

--- app/src/androidTest/java/br/com/alura/ceep/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/alura/ceep/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.ceep;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
---
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,31 @@
+package br.com.alura.ceep.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
+
+import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+
+        ListView listaDeNotas = findViewById(R.id.listView);
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira Nota", "Primeira Descrição"));
+
+        List<Nota> notas = dao.todos();
+
+        listaDeNotas.setAdapter(new ListaNotasAdapter(this, notas));
+    }
+}
