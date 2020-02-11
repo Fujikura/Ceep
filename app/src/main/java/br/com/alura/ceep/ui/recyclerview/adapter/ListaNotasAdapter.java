01b4d83014b605d55af39a20cbafc7f6f883f4ac
Marcio <marciofujikura@hotmail.com>
2020-02-11 19:26:14 -0300

    implementando boas praticas

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,8 +12,9 @@ import java.util.List;
 
 import br.com.alura.ceep.R;
 import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.recyclerview.viewholder.NotaViewHolder;
 
-public class ListaNotasAdapter extends RecyclerView.Adapter {
+public class ListaNotasAdapter extends RecyclerView.Adapter<NotaViewHolder> {
 
     private List<Nota> notas;
     private Context contexto;
@@ -26,32 +26,20 @@ public class ListaNotasAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public NotaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View viewCriada = LayoutInflater.from(contexto)
                 .inflate(R.layout.item_nota, parent, false);
         return new NotaViewHolder(viewCriada);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NotaViewHolder holder, int position) {
         Nota nota = notas.get(position);
-
-        TextView titulo = holder.itemView.findViewById(R.id.item_nota_titulo);
-        titulo.setText(nota.getTitulo());
-
-        TextView descricao = holder.itemView.findViewById(R.id.item_nota_descricao);
-        descricao.setText(nota.getDescricao());
+        holder.vincula(nota);
     }
 
     @Override
     public int getItemCount() {
         return notas.size();
-    }
-
-    class NotaViewHolder extends RecyclerView.ViewHolder {
-
-        public NotaViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
     }
 }
---
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/viewholder/NotaViewHolder.java
@@ -0,0 +1,30 @@
+package br.com.alura.ceep.ui.recyclerview.viewholder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import br.com.alura.ceep.R;
+import br.com.alura.ceep.model.Nota;
+
+public class NotaViewHolder extends RecyclerView.ViewHolder {
+
+    private final TextView titulo;
+    private final TextView descricao;
+
+    public NotaViewHolder(@NonNull View itemView) {
+        super(itemView);
+        titulo = itemView.findViewById(R.id.item_nota_titulo);
+        descricao = itemView.findViewById(R.id.item_nota_descricao);
+
+    }
+
+
+    public void vincula(Nota nota){
+        titulo.setText(nota.getTitulo());
+        descricao.setText(nota.getDescricao());
+
+    }
+}
