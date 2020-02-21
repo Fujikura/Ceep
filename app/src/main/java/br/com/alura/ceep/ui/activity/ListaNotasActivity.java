63c0a8eba6780421df7b399c3dd04aecb0c571fd
Marcio <marciofujikura@hotmail.com>
2020-02-20 21:47:50 -0300

    removendo notas de exemplo

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -85,14 +85,7 @@ public class ListaNotasActivity extends AppCompatActivity {
 
 
     private List<Nota> pegaTodasNotas() {
-        NotaDAO dao = new NotaDAO();
-        for (int i = 0; i < 10; i++) {
-            dao.insere(new Nota(
-                    "Titulo " + (i + 1),
-
-                    "Descrição " + (i + 1)));
-        }
-        return dao.todos();
+        return new NotaDAO().todos();
     }
 
     private void alteraNota(Nota nota, int posicao) {
